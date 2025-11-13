5b5bd6a7c0b1a6a0d028c5d463b5e994dc7876c7
Salman Husain <shusain@nrel.gov>
2025-11-13 14:10:38 -0700

    polish_mesher

--- dolphin/assets/design_recipe/mesher.py
+++ dolphin/assets/design_recipe/mesher.py
@@ -2,16 +2,19 @@ import pygmsh
 import numpy as np
 
 
-def auv_mesh(Length, radius):
+def auv_mesh(Length, radius, resolution):
     ""
 
 
     with pygmsh.occ.Geometry() as geom:
     
-        body    = geom.add_cylinder([0,0,0], [0,0,-Length], radius, angle=2 * np.pi)
+        body    = geom.add_cylinder([0,0,0], [0,0,-Length], 
+                                    radius, 
+                                    angle=2 * np.pi,
+                                    mesh_size=resolution)
         body.dim_tag = [3,1]
 
-        head    = geom.add_ball([0, 0, -Length], radius, 0,mesh_size = 0.1)
+        head    = geom.add_ball([0, 0, -Length], radius, 0,mesh_size = resolution)
         geom.rotate(head, [0,0,-Length,],1*np.pi,[0,1,0]) 
         head.dim_tag = [3,2]
     
@@ -19,17 +22,30 @@ def auv_mesh(Length, radius):
         auv = geom.generate_mesh()
         auv.write('auv_mesh.stl')
 
-def flap(corner, width, depth, thickness, name):
+def flap(corner, thickness, width, depth, name, resolution):
     ""
     with pygmsh.occ.Geometry() as geom:
     
-        extends = [width, depth, thickness]  # Size in x, y, z directions
-        panel = geom.add_box(corner, extends, mesh_size=0.1)
+        extends = [thickness, width, depth]  # Size in x, y, z directions
+        panel = geom.add_box(corner, extends, mesh_size=resolution)
         flap  = geom.generate_mesh()
         flap.write(f"{name}.stl")
 
 
 if __name__ == "__main__":
-    auv_mesh(10, 1)
-    flap([0, 0.5,0], .125,1,-2,'flap_left')
-    flap([0,-1.5,0], .125,1,-2,'flap_right')
+    
+    auv_length  = 10    # AUV Length
+    auv_radius  = 1     # AUV Radius
+    depth       = 4     # Flap Depth (- z axis)
+    width       = 4     # Flap Width (+ y-axis)
+    thickness   = 0.125 # Flap Thickness (+- x-axis)
+    resolution  = 0.1
+    auv_mesh(auv_length, auv_radius, resolution)
+
+    # flap(corner,thickness, width, depth, name)
+    flap([0, auv_radius,0], 
+         thickness, width, -depth,
+         'flap_left', 5*resolution) 
+    flap([0,-(auv_radius+width),0], 
+         thickness, width, -depth,
+         'flap_right', 5*resolution)
